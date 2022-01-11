6f24c83bb33e92dc87ceb3a0f1723133f5be3927
br3nds0n <brendson.net@gmail.com>
2022-01-10 23:32:23 -0300

    feat: create list

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
+import { Negociacoes } from "../models/negociacoes.js";
 import { Negociacao } from "../models/nogociacao.js";
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
+  private negociacoes: Negociacoes = new Negociacoes();
 
   constructor() {
     this.inputData = document.querySelector('#data');
@@ -12,8 +14,10 @@ export class NegociacaoController {
   } 
 
   adiciona(): void {
-    const negociacao = this.criaNegociacao;
-    console.log(negociacao);
+    const negociacao = this.criaNegociacao();
+    this.negociacoes.adiciona(negociacao);
+    this.negociacoes.lista();
+    console.log(this.negociacoes.lista());
     this.limparFormulario();
   }
 
